b6f21a0da02054762349065814e107fdb38ea7c1
Khang Huynh <khanghuynh92@gmail.com>
2017-08-26 20:27:03 +0700

    add scraper-id header

--- helpers/Upload.js
+++ helpers/Upload.js
@@ -1,18 +1,22 @@
+import Fs from 'fs';
 import Fetch from 'node-fetch';
 
 const upload = async (posts) => {
-  try {
-    await Fetch(`${process.env.SCRAPER_ADMIN_URL}/upload`, {
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-      method: 'POST',
-      body: JSON.stringify(posts),
-    });
-    console.log('Upload successfully!');
-  } catch (err) {
-    console.log(err);
+  const scraper = JSON.parse(Fs.readFileSync('./scraper.json', 'utf8'));
+  if (scraper.id) {
+    try {
+      await Fetch(`${process.env.SCRAPER_ADMIN_URL}/upload`, {
+        headers: {
+          'Content-Type': 'application/json',
+          'scraper-id': scraper.id,
+        },
+        method: 'POST',
+        body: JSON.stringify(posts),
+      });
+      console.log('Upload successfully!');
+    } catch (err) {
+      console.log(err);
+    }
   }
 };
 
